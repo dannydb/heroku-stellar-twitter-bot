176ced05b2f1152056099417641d9aa9318e8bfd
Danny DeBelius <danny.debelius@gmail.com>
2013-03-22 16:12:38 -0400

    Use JSON feed instead of RSS, check for three favorites before pushing to Twitter

--- stellar2twitter.rb
+++ stellar2twitter.rb
@@ -10,21 +10,24 @@ Twitter.configure do |c|
   c.oauth_token_secret = ENV["OAUTH_TOKEN_SECRET"]
 end
 
-feed_url = "http://stellar.io/#{ENV["USERNAME"]}/flow/feed"
+feed_url = "http://stellar.io/#{ENV["USERNAME"]}/flow/json"
 
 OEmbed::Providers.register_all
 client = Twitter::Client.new
-feed = RSS::Parser.parse(open(feed_url))
+feed = JSON.parse(open(feed_url).read)
 
-feed.items.each do |item|
-  url = item.link.href
+feed['items'].each do |item|
+  url = item['item_url']
+  favecount = item['favedby_entities'].length
+  next if favecount < 3
+
   begin
     if url.match(/twitter.com\/.*\/status\/(.*)$/)
       client.retweet($1)
-    else
-      info = OEmbed::Providers.get(url)
-      url = info.web_page_short_url unless info.provider_name != "Flickr"
-      client.update("#{info.author_name}: #{url}")
+    # else
+    #   info = OEmbed::Providers.get(url)
+    #   url = info.web_page_short_url unless info.provider_name != "Flickr"
+    #   client.update("#{info.author_name}: #{url}")
     end
   rescue Exception => e
     puts "Oh well: #{e}"
